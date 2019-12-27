d8cdec1a1838568e5d16f9d19353f41507cb5a15
sermicromegas <serguardians@libero.it>
2019-12-27 13:39:30 +0100

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const graphqlHttp = require('express-graphql');
+
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: true,
+    formatError(err) {
+      if (!err.originalError) {
+        return err;
+      }
+      const data = err.originalError.data;
+      const message = err.message || 'An error occurred.';
+      const code = err.originalError.code || 500;
+      return { message: message, status: code, data: data };
+    }
+  })
+);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_URI)
+  .then(result => {
+    app.listen(process.env.PORT || 3000);
+  })
+  .catch(err => console.log(err));
---
+++ backend/graphql/schema.js
@@ -0,0 +1,53 @@
+const { buildSchema } = require('graphql');
+
+module.exports = buildSchema(`
+    type Course {
+        _id: ID!
+        title: String!
+        subtitle: String!
+        description: String!
+        imageUrl: String!
+        rating: Int
+        price: Float
+        sections: [Section!]!
+        createdAt: String!
+        updatedAt: String!
+    }
+
+    type Section {
+        _id: ID!
+        title: String!
+        lessons: [Lesson!]!
+    }
+
+    type Lesson {
+        _id: ID!
+        title: String!
+        duration: String!
+        videoUrl: String!
+        isFree: Boolean
+        completed: Boolean
+    }
+
+    input CourseInputData {
+        title: String!
+        subtitle: String!
+        description: String!
+        imageUrl: String!
+        rating: Int
+        price: Float
+    }
+
+    type RootQuery {
+        courses: [Course]!
+    }
+
+    type RootMutation {
+        createCourse(courseInput: CourseInputData!): Course!
+    }
+
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+`);
